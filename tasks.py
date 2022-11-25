bd49b9ad26d1bfb75a296d21d98e06b0efef50ec
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-11-25 16:09:49 +0200

    add invoke-tasks

---
+++ tasks.py
@@ -0,0 +1,31 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("python3 src/launch.py", pty=True)
+
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html", pty=True)
+
+
+@task
+def build(ctx):
+    ctx.run("python3 src/build.py", pty=True)
+
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
