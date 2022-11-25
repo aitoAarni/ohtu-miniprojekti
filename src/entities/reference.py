898513d69d932d9122150a37334d28e82c317cdc
Aarni <dabeastelele@gmail.com>
2022-11-25 15:46:59 +0200

    ArticleReference class added

---
+++ src/entities/reference.py
@@ -0,0 +1,10 @@
+class ArticleReference:
+    def __init__(self) -> None:
+        self.fields = {
+            "author": None,
+            "title": None,
+            "journal": None,
+            "year": None,
+            "volume": None,
+            "pages": None
+        }
