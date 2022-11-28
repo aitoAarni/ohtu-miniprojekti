667aadad021f4e3f06fc035061e363d23578e1e0
Samu Syrjänen <samu.syrjanen@gmail.com>
2022-11-28 11:47:11 +0200

    edited service

--- src/index.py
+++ src/index.py
@@ -1,11 +1,10 @@
 from interface import Interface
 from services.reference_service import ReferenceService
-from repositories.reference_repository import ReferenceRepository
+from repositories.reference_repository import default_reference_repository
 
 
 def main():
-    reference_repository = ReferenceRepository()
-    reference_service = ReferenceService(reference_repository)
+    reference_service = ReferenceService(default_reference_repository)
     interface = Interface(reference_service)
 
     interface.start()
--- src/services/reference_service.py
+++ src/services/reference_service.py
@@ -1,18 +1,18 @@
 from entities.reference import Reference
-from repositories.reference_repository import default_reference_repository
 
 class ReferenceService:
     def __init__(self, reference_repository):
         self.reference_repository = reference_repository
     
     def get_template_reference(self):
-        return Reference()
+        self.reference = Reference()
+        return self.reference.fields
 
-    def save_reference(self, reference :Reference):
-        self.reference_repository.add_reference(reference)
+    def save_reference(self, fields_dict :dict):
+        for key, value in fields_dict.items():
+            self.reference.fields[key] = value
+        self.reference_repository.add_reference(self.reference)
 
-    def list_references(self):
-        references = default_reference_repository.get_all()
-
-        for reference in references:
-            print(reference)
+    def get_all_references(self):
+        references = self.reference_repository.get_all()
+        return references
