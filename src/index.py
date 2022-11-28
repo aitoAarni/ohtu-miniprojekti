6046d14a788effa4f0a93e971962ed10345fc6dd
Antti Romppanen <romppanen.antti@gmail.com>
2022-11-28 23:54:55 +0200

    add reference listing

--- src/index.py
+++ src/index.py
@@ -1,11 +1,13 @@
 from interface import Interface
 from services.reference_service import ReferenceService
 from repositories.reference_repository import default_reference_repository
+from user_input import UserInput
 
 
 def main():
     reference_service = ReferenceService(default_reference_repository)
-    interface = Interface(reference_service)
+    user_io = UserInput()
+    interface = Interface(reference_service, user_io)
 
     interface.start()
 
--- src/interface.py
+++ src/interface.py
@@ -1,16 +1,19 @@
 
 class Interface:
-    def __init__(self, reference_service):
+    def __init__(self, reference_service, user_input):
         self.reference_service = reference_service
+        self.user_io = user_input
 
     def start(self):
         while True:
-            command = input("Enter command: (new, list, exit): ").lower()
+            command = self.user_io.input_reference(
+                "Enter command: (new, list, exit): ").lower()
 
             if command == "exit":
-                print("Exit selected, goodbye")
+                self.user_io.output_reference("Exit selected, goodbye")
                 break
 
+            print("")
             self.command_selector(command)
 
     def command_selector(self, command):
@@ -18,14 +21,33 @@ class Interface:
             fields_dict = self.new_reference()
             self.reference_service.save_reference(fields_dict)
         elif command == "list":
-         #   print("Not implemented yet")
-            return self.reference_service.list_references()
+            self.list_references()
 
     def new_reference(self):
         fields_dict = self.reference_service.get_template_reference()
 
         for field in fields_dict:
-            user_input = input(f"\t Enter {field}: ")
+            user_input = self.user_io.input_reference(f"\t Enter {field}: ")
             fields_dict[field] = user_input
 
         return fields_dict
+
+    def list_references(self):
+        references = self.reference_service.get_all_references()
+
+        for reference in references:
+            self.print_reference_objects(reference)
+
+    def print_reference_objects(self, reference):
+        last_key = list(reference.keys())[-1]
+
+        for key, value in reference.items():
+            if not value:
+                continue
+
+            if key == "citekey":
+                self.user_io.output_reference(f"{key} {value}")
+            elif key == last_key:
+                self.user_io.output_reference(f"  └── {key}: {value} \n")
+            else:
+                self.user_io.output_reference(f"  ├── {key}: {value}")
