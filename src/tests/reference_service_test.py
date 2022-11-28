3dd91a37fe2f0fc63d647d4854a7e73876e30116
Samu Syrjänen <samu.syrjanen@gmail.com>
2022-11-28 12:38:21 +0200

    service tests modified

--- src/tests/reference_service_test.py
+++ src/tests/reference_service_test.py
@@ -1,14 +1,34 @@
 import unittest
 from unittest.mock import Mock, ANY
 from services.reference_service import ReferenceService
+from repositories.reference_repository import ReferenceRepository
 
 
 class TestReferenceService(unittest.TestCase):
-    def test_save_reference_calls_repository_with_correct_parameters(self):
-        reference_repository_mock = Mock()
-        reference_mock = Mock()
-        reference_service = ReferenceService(reference_repository_mock)
+    def setUp(self):
+        reference_repository_mock = Mock(wraps=ReferenceRepository())
+        self.reference_service = ReferenceService(reference_repository_mock)
 
-        reference_service.save_reference(reference_mock)
+    def test_save_reference_creates_correct_dict(self):
+        reference_correct_fields = {
+            'address': None,
+            'author': None,
+            'citekey': None,
+            'edition': None,
+            'journal': None,
+            'month': None,
+            'note': None,
+            'number': None,
+            'pages': None,
+            'publisher': None,
+            'series': None,
+            'title': 'test',
+            'volume': None,
+            'volume_or_number': None,
+            'year': None
+        }
 
-        reference_repository_mock.add_reference.assert_called_with(reference_mock)
+        self.reference_service.get_template_reference()
+        self.reference_service.save_reference({'title': 'test'})
+
+        self.assertEqual(self.reference_service.reference.fields['title'], reference_correct_fields['title'])
