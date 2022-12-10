912f9ffcdf478b522ab423bbe747cfd491f9dde5
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-12-10 09:49:11 +0200

    initial commit

---
+++ src/services/bib_import.py
@@ -0,0 +1,37 @@
+from .file_management import default_file_management
+from entities.reference import Reference
+
+
+class BibImport:
+
+    def __init__(self):
+        """Imports bib-files created with this application. 
+        """
+        self.filemanager = default_file_management
+
+    def create_reference_entities(self, filename: str):
+        content = self.filemanager.read_document(filename)
+
+        list_of_references = []
+        reference = ""
+        for row in content.splitlines():
+            if len(row) > 1:
+                reference += ";" + row
+            elif len(reference) > 1:
+                list_of_references.append(reference)
+                reference = ""
+
+        references = []
+        for item in list_of_references:
+            ref = item.split(";")
+            reference = Reference()
+            for row in ref:
+                if not row:
+                    continue
+                if row[0] == "@":
+                    parts = row.split("{")
+                    reference.set_field("citekey", parts[1])
+                elif len(row) > 1:
+                    parts = row.split("=")
+                    reference.set_field(parts[0][4:-1], parts[1][2:-2])
+            references.append(reference)
