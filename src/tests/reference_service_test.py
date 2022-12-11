d33119e1ae2d4c6d5230981912e7d3e509e4cb70
Iida Björn <iida.w.bjorn@gmail.com>
2022-12-11 20:20:48 +0200

    useless test deleted

--- src/tests/reference_service_test.py
+++ src/tests/reference_service_test.py
@@ -85,6 +85,3 @@ class TestReferenceService(unittest.TestCase):
     def test_get_importable_files_returns_a_list(self):
         file_list = self.reference_service.get_importable_files()
         self.assertEqual(type(file_list), list)
-
-    def test_search_references(self):
-        pass
